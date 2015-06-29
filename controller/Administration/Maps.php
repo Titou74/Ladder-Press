319b8206b6c05569f9093b7b4c134bc309fc0b5d
T Dupuy <tdupuy53@gmail.com>
2015-06-29 20:51:33 +0100

    Tezst

--- controller/Administration/Maps.php
+++ controller/Administration/Maps.php
@@ -19,6 +19,7 @@ class MapsAdministration extends WP_List_Table
         
         include_once plugin_dir_path( __FILE__ ).'../../view/template/administration/listMaps.php';
         echo lala;
+        echo test;
     }
     
     /**
