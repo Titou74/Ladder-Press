8b9472017322e322593427bb29097776a7ba6cd3
Titou <Titou@PC-Titou.home>
2015-07-15 21:10:44 +0200

    Ajout de la gestion des apostrophes

---
+++ controller/Users.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Users
+ *
+ * @author Titou
+ */
+class Users {
+    public function userMenu() {
+        if(!isset($_GET['page'])) {
+            
+        } else if($_GET['page'] == "gameData_list") {
+            
+        } else if($_GET['page'] == "gameData_add") {
+            
+        } else if($_GET['page'] == "gameData_edit") {
+            
+        }
+    }
+}
