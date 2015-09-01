b099ced8f5abec717971e30fed0d64643257502e
Titou74 <titou.c.74@gmail.com>
2015-09-01 18:32:07 +0200

    Mise a jour de la page "User Menu" Add utils directory in model

---
+++ model/utils/UserTeam.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserTeam
+ *
+ * @author Titou74 <titou.c.74@gmail.com>
+ */
+class UserTeam {
+    //put your code here
+}
